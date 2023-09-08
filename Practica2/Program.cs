e2c5dde79754d6a0e7561b32d97491f6d2a344ec
TecNM <TecNM@DESKTOP-BABKH5A>
2023-09-08 12:11:17 -0600

    Agregar archivos de proyecto.

---
+++ Practica2/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("favor de ingresar el diametro");
+double diametro = double.Parse(Console.ReadLine());
+double pi = 3.1416;
+double perimetro = diametro * pi;
+Console.WriteLine("El perimetro es:" + perimetro);
+Console.ReadKey();
